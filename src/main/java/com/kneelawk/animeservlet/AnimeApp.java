8aa95bbebe10b72d8edcf950e215dd84d0c7eab3
Kneelawk <jedidiah.pommert@gmail.com>
2020-02-17 23:42:00 -0800

    Fix potential keystore configuration

--- src/main/java/com/kneelawk/animeservlet/AnimeApp.java
+++ src/main/java/com/kneelawk/animeservlet/AnimeApp.java
@@ -2,6 +2,9 @@ package com.kneelawk.animeservlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
 
 /**
  * Created by Kneelawk on 7/26/19.
@@ -9,6 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnimeApp {
     public static void main(String[] args) {
-        SpringApplication.run(AnimeApp.class, args);
+        SpringApplication application = new SpringApplication(AnimeApp.class);
+        try {
+            application.setDefaultProperties(PropertiesLoaderUtils.loadAllProperties("keystore/keystore.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        application.run(args);
     }
 }
